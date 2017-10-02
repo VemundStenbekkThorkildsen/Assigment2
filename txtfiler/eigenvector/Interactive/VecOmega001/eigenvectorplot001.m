b31023e4daf760695a054bf237055ff29577e995
VemundStenbekkThorkildsen <31346944+VemundStenbekkThorkildsen@users.noreply.github.com>
2017-10-02 23:23:49 +0200

    push

--- txtfiler/eigenvector/Interactive/VecOmega001/eigenvectorplot001.m
+++ txtfiler/eigenvector/Interactive/VecOmega001/eigenvectorplot001.m
@@ -2,21 +2,21 @@ lambda=load('Eigenvec_400_omega(0.01).txt');
 rho=linspace(0,5,400);
 lowestlambda=lambda(1:400).';
 lam1=lowestlambda.^2;
-middlelambda=lambda(401:800).';
-lam2=middlelambda.^2;
-highestlambda=lambda(801:1200).';
-lam3=highestlambda.^2;
+%middlelambda=lambda(401:800).';
+%lam2=middlelambda.^2;
+%highestlambda=lambda(801:1200).';
+%lam3=highestlambda.^2;
 
 figure(1)
 hold on
 plot(rho, lam1)
-plot(rho, lam2)
-plot(rho, lam3)
+%plot(rho, lam2)
+%plot(rho, lam3)
 title( 'Probability distribution plotted against \rho for n=400 and \rho max=5 while \omega_r=0.01')
 xlabel ('\rho')
 ylabel ( '\psi^2');
-legend('\lambda_1','\lambda_2','\lambda_3')
-
+legend('\lambda_1')
+%%
 lambda=load('Eigenvec_40_omega(0.01).txt');
 rho=linspace(0,5,40);
 lowestlambda=lambda(1:40).';
