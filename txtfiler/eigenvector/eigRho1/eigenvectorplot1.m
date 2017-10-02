d6c2685b69a0ab50f298c27285faff1ec2491053
VemundStenbekkThorkildsen <31346944+VemundStenbekkThorkildsen@users.noreply.github.com>
2017-10-02 10:58:27 +0200

    matlabfiler og figurer

--- txtfiler/eigenvector/eigRho1/eigenvectorplot1.m
+++ txtfiler/eigenvector/eigRho1/eigenvectorplot1.m
@@ -1,12 +1,10 @@
-
-
-lambda=load('Eigenvector_360_10.000000.txt');
-rho=linspace(0,10,360);
-lowestlambda=lambda(1:360).';
+lambda=load('Eigenvector_400_1.000000.txt');
+rho=linspace(0,1,400);
+lowestlambda=lambda(1:400).';
 lam1=lowestlambda.^2;
-middlelambda=lambda(361:720).';
+middlelambda=lambda(401:800).';
 lam2=middlelambda.^2;
-highestlambda=lambda(721:1080).';
+highestlambda=lambda(801:1200).';
 lam3=highestlambda.^2;
 
 figure(1)
@@ -14,13 +12,13 @@ hold on
 plot(rho, lam1)
 plot(rho, lam2)
 plot(rho, lam3)
-title( 'Probability distribution plotted against \rho for n=360 \rho max=10')
+title( 'Probability distribution plotted against \rho for n=400 and \rho max =1')
 xlabel ('rho')
 ylabel ( '\psi^2');
 legend('lam1','lam2','lam3')
 
-lambda=load('Eigenvector_360_10.000000.txt');
-rho=linspace(0,10,40);
+lambda=load('Eigenvector_40_1.000000.txt');
+rho=linspace(0,1,40);
 lowestlambda=lambda(1:40).';
 lam1=lowestlambda.^2;
 middlelambda=lambda(41:80).';
@@ -33,8 +31,7 @@ hold on
 plot(rho, lam1)
 plot(rho, lam2)
 plot(rho, lam3)
-title( 'Probability distribution plotted against \rho for n=40 and \rho max=10')
+title( 'Probability distribution plotted against \rho for n=40 and \rho max=1')
 xlabel ('rho')
 ylabel ( '\psi^2');
 legend('lam1','lam2','lam3')
-
